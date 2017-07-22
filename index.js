bbf4c13592ae3d643fc13375c519c1f384d115b0
Peter Forgacs <peter.forgacs3@gmail.com>
2017-07-22 21:04:39 +0200

    Rename app to index

--- index.js
+++ index.js
@@ -4,34 +4,37 @@ const fsx   	 = require('fs-extra');
 const argv       = require('minimist')(process.argv.slice(2));
 const parse      = require('url-parse');
 const path       = require('path');
-const exec 		 = require('child_process').spawn;
+const exec 		 = require('child_process').exec;
+const spawn 	 = require('child_process').spawn;
 
 /*---- Validate ---------------------------------------------------
 *   In case its used as a standalone program.
 *------------------------------------------------------------------*/
-if (!argv.urls || !argv.cookies){
-	console.log('Missing arguments.');
+if (!argv.urls || !argv.username || !argv.password){
+	console.log('Missing arguments: node app --username=xxx --password=xxx --url=url');
 	process.exit(1);
 }
 
+let baseDir = process.cwd();	// The path the process is being called on
+if (argv.download){
+	baseDir = argv.download;
+}
+
 module.exports = class LyndaDownloader {
-	constructor(cookies, urls){
-		this.validate(cookies, urls);
+	constructor(username, password){
+		this.username = username;
+		this.password = password;
 		this.urls    = [];
-		this.cookies =  cookies;
 	}
 
-	validate(cookies, urls){
-		fsx.ensureFile(cookies)
-			.then( () => {
-			fsx.ensureFile(urls);
-			})
-			.catch( error => {
-				console.log(error);
-				console.log('Not valid files.');
-				process.exit(1);
-			});
-		}
+	validate(file){
+		fsx.ensureFile(file)
+		.catch( error => {
+			console.log(error);
+			console.log('Not valid files.');
+			process.exit(1);
+		});
+	}
 
 	setUrl(urls){
 		return new Promise ( (resolve, reject) => { 
@@ -40,13 +43,14 @@ module.exports = class LyndaDownloader {
 					reject('Invalid URL.');
 				}
 
+			this.validate(urls);
 			fs.readFile(urls, (err, data) => {
 				if(err) throw err;
 				var lines = data.toString().split("\n");
 				lines.forEach( line => {
 					this.urls.push(this.parse(line));
+					console.log(`Added URL: ${line}`);
 				}, this);
-				console.log(this.urls);
 				resolve();
 			});
 		});
@@ -77,29 +81,36 @@ module.exports = class LyndaDownloader {
 
 	download(url){
 		return new Promise ( (resolve, reject) => {
-			let downloadPath = __dirname + url.dir;	// /home/peter/nodejs/lynda-downloader/Software-Development-tutorials/Foundations-Programming-Web-Services
-			let cookiesPath;
+			let downloadPath = process.cwd() + url.dir;	// /home/peter/nodejs/lynda-downloader/Software-Development-tutorials/Foundations-Programming-Web-Services
 
 			return fsx.ensureDir(downloadPath)
 			.then( () => {
-				
+
 				process.chdir(downloadPath);
 				
-				if(path.isAbsolute(this.cookies)) {
-					 cookiesPath = this.cookies;
-				} else {
-					cookiesPath = __dirname + '/' + this.cookies;
-				}
+				let args = [
+					'--all-subs',  '-o', 
+					'"%(playlist_index)s-%(title)s.%(ext)s"',  url,
+					'--username', this.username,
+					'--password', this.password
+				];
+				
+				console.log(args);
+				let youtube = spawn('youtube-dl', args );
 
-				// TODO: Use spawn https://nodejs.org/api/child_process.html#child_process_child_process_spawn_command_args_options
-				exec(`youtube-dl --cookies ${cookiesPath} --all-subs -o "%(playlist_index)s-%(title)s.%(ext)s" ${url}`, (error, stdout) => {
-				if (error) {
-					console.log(error);
+				youtube.stdout.on('data', (data) => {
+					console.log(data.toString('utf8'));
+				});
+
+				youtube.stderr.on('data', (error) => {
+					console.log(error.toString('utf8'));
 					reject(error);
-				}
+				});
 
-				process.chdir(__dirname);
-				resolve();		
+				youtube.on('close', (code) => {
+					console.log(`Closing download.`);
+					process.chdir(baseDir);
+					resolve();		
 				});
 			});
 		});
@@ -109,7 +120,7 @@ module.exports = class LyndaDownloader {
 /*---- Example ----------------------------------------------------
 *   Test download run.
 *-------------------------------------------------------------------*/
-let run = new module.exports(argv.cookies, argv.urls);
+let run = new module.exports(argv.username, argv.password);
 run.setUrl(argv.urls)
 .then( () => {
    run.start();
